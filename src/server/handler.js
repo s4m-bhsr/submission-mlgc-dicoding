39c9bb6d1cbb3502b7264b770561111aae54f5ef
Samuel <samuelbhasarie.education@gmail.com>
2024-05-22 04:20:31 +0800

    Push code to remote repository

--- src/server/handler.js
+++ src/server/handler.js
@@ -2,23 +2,23 @@ const predictClassification = require('../services/inferenceService');
 const crypto = require('crypto');
 const storeData = require('../services/storeData');
 
-
 async function postPredictHandler(request, h) {
   const { image } = request.payload;
   const { model } = request.server.app;
 
-  const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
+  const { confidenceScore, label, explanation, suggestion } =
+    await predictClassification(model, image);
   const id = crypto.randomUUID();
   const createdAt = new Date().toISOString();
 
   const data = {
-    "id": id,
-    "result": label,
-    "suggestion": suggestion,
-    "createdAt": createdAt
-  }
+    id: id,
+    result: label,
+    suggestion: suggestion,
+    createdAt: createdAt,
+  };
 
-  await storeData(id,data);
+  await storeData(id, data);
 
   const response = h.response({
     status: 'success',
@@ -29,4 +29,4 @@ async function postPredictHandler(request, h) {
   return response;
 }
 
-module.exports = postPredictHandler; 
+module.exports = postPredictHandler;
